845aa4ce39952dc267ad5eb6655421d4ae9c077e
Bilal Qadri <bilalattari1409l@gmail.com>
2024-05-11 10:06:57 +0500

    variables learned

---
+++ variables.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greeting = 'Hello Bilal';
+console.log(greeting);
---
+++ variables.ts
@@ -0,0 +1,5 @@
+let greeting: string = 'Hello Bilal'
+
+console.log(greeting)
+
+export{}
